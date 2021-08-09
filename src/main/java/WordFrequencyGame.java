ac89d9d32d039d85bdb4a089eb5826d4f24272fc
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-09 15:46:27 +0800

    [WordFrequency][Mark] refactor: Removed else statement and added isInputOneWord method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -7,47 +7,43 @@ public class WordFrequencyGame {
 
 
     public String getResult(String sentence) {
-
-
-        if (sentence.split(WHITE_SPACES).length == 1) {
+        if (isInputOneWord(sentence)) {
             return sentence + " 1";
-        } else {
-
-            try {
-
-                //split the input string with 1 to n pieces of spaces
-                String[] words = sentence.split(WHITE_SPACES);
-
-                List<WordInfo> wordInfoList = new ArrayList<>();
-                for (String word : words) {
-                    WordInfo wordInfo = new WordInfo(word, 1);
-                    wordInfoList.add(wordInfo);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<WordInfo>> map = getListMap(wordInfoList);
-
-                List<WordInfo> list = new ArrayList<>();
-                for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
-                    WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-                    list.add(wordInfo);
-                }
-                wordInfoList = list;
-
-                wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (WordInfo w : wordInfoList) {
-                    String s = w.getValue() + " " + w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-
-
-                return CALCULATE_ERROR;
-            }
         }
+        try {
+            //split the input string with 1 to n pieces of spaces
+            String[] words = sentence.split(WHITE_SPACES);
+
+            List<WordInfo> wordInfoList = new ArrayList<>();
+            for (String word : words) {
+                WordInfo wordInfo = new WordInfo(word, 1);
+                wordInfoList.add(wordInfo);
+            }
+
+            //get the map for the next step of sizing the same word
+            Map<String, List<WordInfo>> map = getListMap(wordInfoList);
+
+            List<WordInfo> list = new ArrayList<>();
+            for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
+                WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
+                list.add(wordInfo);
+            }
+            wordInfoList = list;
+
+            wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+
+            StringJoiner joiner = new StringJoiner("\n");
+            for (WordInfo w : wordInfoList) {
+                String s = w.getValue() + " " + w.getWordCount();
+                joiner.add(s);
+            }
+            return joiner.toString();
+        } catch (Exception e) {
+
+
+            return CALCULATE_ERROR;
+        }
+
     }
 
 
@@ -66,6 +62,10 @@ public class WordFrequencyGame {
 
 
         return map;
+    }
+
+    public boolean isInputOneWord(String sentence){
+        return sentence.split(WHITE_SPACES).length == 1;
     }
 
 
