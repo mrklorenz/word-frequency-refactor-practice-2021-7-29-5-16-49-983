7301511f876901cfc02f38528b3f4d01416a55e5
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-09 20:22:43 +0800

    [WordFrequency][Mark] refactor: Added method joinWordInfoList that appends the result from a list

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class WordFrequencyGame {
 
     public static final String WHITE_SPACES = "\\s+";
-    public static final String CALCULATE_ERROR = "Calculate Error";
+    public static final String CALCULATE_ERROR_MSG = "Calculate Error";
 
 
     public String getResult(String sentence) {
@@ -17,44 +17,32 @@ public class WordFrequencyGame {
             wordInfoList = calculateWordFrequency(wordInfoListMap);
             sortWordInfoListByCount(wordInfoList);
 
-            StringJoiner joiner = new StringJoiner("\n");
-            for (WordInfo w : wordInfoList) {
-                String s = w.getValue() + " " + w.getWordCount();
-                joiner.add(s);
-            }
-            return joiner.toString();
+            return joinWordInfoList(wordInfoList);
         } catch (Exception e) {
-            return CALCULATE_ERROR;
+            return CALCULATE_ERROR_MSG;
         }
-
     }
 
-    public void sortWordInfoListByCount(List<WordInfo> wordInfoList){
+    public String joinWordInfoList(List<WordInfo> wordInfoList) {
+        StringJoiner joiner = new StringJoiner("\n");
+        for (WordInfo wordInfo : wordInfoList) {
+            String currentWordInfo = wordInfo.getValue() + " " + wordInfo.getWordCount();
+            joiner.add(currentWordInfo);
+        }
+        return joiner.toString();
+    }
+
+    public void sortWordInfoListByCount(List<WordInfo> wordInfoList) {
         wordInfoList.sort((firstWord, nextWord) -> nextWord.getWordCount() - firstWord.getWordCount());
     }
 
-    public List<WordInfo> calculateWordFrequency(Map<String, List<WordInfo>> wordInfoListMap){
+    public List<WordInfo> calculateWordFrequency(Map<String, List<WordInfo>> wordInfoListMap) {
         List<WordInfo> wordInfoList = new ArrayList<>();
         for (Map.Entry<String, List<WordInfo>> listOfWordInfo : wordInfoListMap.entrySet()) {
             WordInfo wordInfo = new WordInfo(listOfWordInfo.getKey(), listOfWordInfo.getValue().size());
             wordInfoList.add(wordInfo);
         }
         return wordInfoList;
-    }
-
-
-    private Map<String, List<WordInfo>> getMapOfWordInfoList(List<WordInfo> wordInfoList) {
-        Map<String, List<WordInfo>> map = new HashMap<>();
-        for (WordInfo wordInfo : wordInfoList) {
-            if (!map.containsKey(wordInfo.getValue())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordInfo);
-                map.put(wordInfo.getValue(), arr);
-            } else {
-                map.get(wordInfo.getValue()).add(wordInfo);
-            }
-        }
-        return map;
     }
 
     public boolean isInputOneWord(String sentence) {
@@ -70,4 +58,17 @@ public class WordFrequencyGame {
         return wordInfoList;
     }
 
+    private Map<String, List<WordInfo>> getMapOfWordInfoList(List<WordInfo> wordInfoList) {
+        Map<String, List<WordInfo>> map = new HashMap<>();
+        for (WordInfo wordInfo : wordInfoList) {
+            if (!map.containsKey(wordInfo.getValue())) {
+                ArrayList arr = new ArrayList<>();
+                arr.add(wordInfo);
+                map.put(wordInfo.getValue(), arr);
+            } else {
+                map.get(wordInfo.getValue()).add(wordInfo);
+            }
+        }
+        return map;
+    }
 }
